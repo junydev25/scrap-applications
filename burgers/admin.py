3cccf23825ab4c7e11fd82aa5d1f923f33b8d5d0
junydev25 <junydev25@gmail.com>
2025-09-15 15:00:41 +0900

    Init: Add pyburger projects

---
+++ burgers/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from burgers.models import Burger
+
+@admin.register(Burger)
+class BurgerAdmin(admin.ModelAdmin):
+    pass
+
+
+# Register your models here.
---
+++ config/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from burgers.models import Burger
+def main(request):
+    return render(request, "main.html")
+
+def burger_list(request):
+    burgers = Burger.objects.all()
+
+    context = {
+        "burgers": burgers
+    }
+
+    return render(request,"burger_list.html", context)
+
+def burger_search(request):
+    keyword = request.GET.get("keyword")
+
+    if keyword is not None:
+        burgers = Burger.objects.filter(name__contains=keyword)
+    else:
+        burgers = Burger.objects.none()
+
+    context = {
+        "burgers": burgers,
+    }
+
+    return render(request, "burger_search.html", context)
